73d92cf003752642adc18821936c4ad613b08e2f
thomas <thomas.billaud@epitech.eu>
2020-03-05 14:25:30 +0100

    [MOD] : More error handling, now check if the map has only valid characters in it

--- solver/src/fill_arr.c
+++ solver/src/fill_arr.c
@@ -8,6 +8,18 @@
 #include "my.h"
 #include <string.h>
 
+int valid_line(char *buff)
+{
+    int i = 0;
+
+    while (buff[i] != '\0') {
+        if (buff[i] != 'X' && buff[i] != '*')
+            return 84;
+        i++;
+    }
+    return 0;
+}
+
 char **fill_arr(int fd)
 {
     char **maze = NULL;
@@ -20,7 +32,7 @@ char **fill_arr(int fd)
     buff = get_next_line(fd);
     size = strlen(buff);
     while (buff != NULL) {
-        if ((int)strlen(buff) != size)
+        if ((int)strlen(buff) != size || valid_line(buff) != 0)
             return NULL;
         maze = realloc(maze, sizeof(char *) * (x + 2));
         maze[x] = malloc(sizeof(char) * (strlen(buff) + 1));
